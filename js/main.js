055031be62778cb0b67ab77171df333a8c3284ed
Althaf ma <althaf@spawoz.com>
2021-11-28 20:07:16 +0530

    Changes in experice and education

--- js/main.js
+++ js/main.js
@@ -1,14 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    let myAge = getAge();
 
-    $('#menu').click(function(){
+    $('#myAge').html(myAge);
+
+    $('#menu').click(function () {
         $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle')
-    })
+        $('header').toggleClass('toggle');
+    });
 
-    $(window).on('scroll load', function(){
+    $(window).on('scroll load', function () {
+
         $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle')
-    })
+        $('header').removeClass('toggle');
 
-})
+        if ($(window).scrollTop() > 0) {
+            $('.top').show();
+        } else {
+            $('.top').hide();
+        }
+
+    });
+
+    // smooth scrolling 
+
+    $('a[href*="#"]').on('click', function (e) {
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop: $($(this).attr('href')).offset().top,
+
+        },
+            500,
+            'linear'
+        );
+
+    });
+
+
+
+
+});
+
+function getAge(from, to) {
+    let date1 = new Date('1998-01-20');
+    let date2 = new Date(Date.now());
+    let yearsDiff = date2.getFullYear() - date1.getFullYear();
+    return yearsDiff;
+}
